41601b67cff3d3010fb2cf324d180a367240f0cb
Acevedo Kevin <87038684+Kesocial@users.noreply.github.com>
2022-04-14 13:58:25 -0300

    v1.3
    Seccion markers y funcion deleteMarkers()

--- map.js
+++ map.js
@@ -6,4 +6,63 @@ var map = new mapboxgl.Map({
   center: [0, 18], // starting position
   zoom: 5,
 });
-var marker = new mapboxgl.Marker().setLngLat([0, 18]).addTo(map);
+var markers = [];
+var markersRepresentation = [];
+var markersContainer = document.getElementById("markers");
+console.log(map);
+const actualizarMarkers = (markers) => {
+  markersContainer.innerHTML = "";
+  markers.forEach(({ longitud, latitud }, index) => {
+    var marker = document.createElement("div");
+    marker.classList.add("markerContenedor");
+    marker.innerHTML = `
+    <div class="marker">
+      <div>
+        Marcador: ${index}
+      </div>
+      <div> ${longitud}</div>
+      <div>${latitud}</div>
+    </div>
+    <button onclick="eliminarMarker(${index})"className="deleteMarker">x</button>
+    `;
+    markersContainer.appendChild(marker);
+  });
+};
+map.on("click", function (e) {
+  var longitud = e.lngLat.lng;
+  var latitud = e.lngLat.lat;
+  var m = new mapboxgl.Marker();
+  m._color = "#fefef";
+  m.setLngLat([longitud, latitud]).addTo(map);
+  const markerObject = {
+    longitud: longitud,
+    latitud: latitud,
+  };
+  markersRepresentation.push(markerObject);
+  markers.push(m);
+  console.log("Marcador agregado");
+  console.log("AllMarkers:");
+  console.log(markers);
+  console.log("AllMarkersContainer:");
+  console.log(markersRepresentation);
+  console.log("MarkerDeleted \n\n");
+  actualizarMarkers(markersRepresentation);
+});
+
+const eliminarMarker = (pos) => {
+  markers[pos].remove();
+  markersRepresentation = markersRepresentation.filter(
+    (e) => e != markersRepresentation[pos]
+  );
+  markers = markers.filter((e) => e != markers[pos]);
+  actualizarMarkers(markersRepresentation);
+  console.log("Marcador eliminado");
+  console.table("Pos:" + pos);
+  console.log("AllMarkers:");
+  console.log(markers);
+  console.log("AllMarkersContainer:");
+  console.log(markersRepresentation);
+  console.log("MarkerDeleted");
+  console.log(markers[pos]);
+  console.log("\n\n");
+};
